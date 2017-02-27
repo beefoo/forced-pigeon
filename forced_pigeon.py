f1eb685cc8780dcbfdad60b34a86902c91b604d8
Brian Foo <brian@youaremyjoy.org>
2017-02-27 13:11:05 -0500

    add preprocess step

--- forced_pigeon.py
+++ forced_pigeon.py
@@ -8,12 +8,15 @@
 #   python forced_pigeon.py -layout large_graph -output output/pigeon_large_graph.png
 #   python forced_pigeon.py -layout fruchterman_reingold -output output/pigeon_fruchterman_reingold.png
 #   python forced_pigeon.py -layout kamada_kawai -output output/pigeon_kamada_kawai.png
+#   python forced_pigeon.py -edges True -output output/pigeon_fruchterman_reingold_user_edges.png
+#   python forced_pigeon.py -edges True -uedges False -output output/pigeon_fruchterman_reingold_edges.png
 
 import argparse
 import collections
 from igraph import *
 import json
 import math
+import os
 from PIL import Image, ImageDraw, ImageFont
 from pprint import pprint
 import random
@@ -25,7 +28,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-layout', dest="LAYOUT_ALGORITHM", default="fruchterman_reingold", help="Layout algorithm, e.g. fruchterman_reingold, kamada_kawai, drl, large_graph")
 parser.add_argument('-output', dest="OUTPUT_FILE", default="output/pigeon_fruchterman_reingold.png", help="Path to output png file")
 parser.add_argument('-edges', dest="EDGES", type=bool, default=False, help="Add edges?")
-
+parser.add_argument('-uedges', dest="USER_EDGES", type=bool, default=True, help="If edges, add just edges?")
 
 # init input
 args = parser.parse_args()
@@ -36,13 +39,14 @@ GRAPH_FILE = "graph/combined-billi.json"
 OUTPUT_FILE = args.OUTPUT_FILE
 DPI = 300
 MARGIN = 1.0 * DPI
-FONT_SIZE = 18
+FONT_SIZE = 16
 FONT_LIGHT = 'Aleo-Light.otf'
 FONT_DARK = 'Aleo-Bold.otf'
 # http://igraph.org/python/doc/tutorial/tutorial.html#layout-algorithms
 # e.g. fruchterman_reingold, grid_fruchterman_reingold, kamada_kawai, drl, large_graph
 LAYOUT_ALGORITHM = args.LAYOUT_ALGORITHM
 EDGES = args.EDGES
+USER_EDGES = args.USER_EDGES
 
 def class2Label(c):
     return c.split(":")[-1].upper()
@@ -64,24 +68,11 @@ pData = list(pImage.getdata())
 (width, height) = pImage.size
 print "Read pigeon image data with dimensions %s x %spx" % ("{:,}".format(width), "{:,}".format(height))
 
-# area of pigon is all the black pixels
-# pigeonArea = len([p for p in pData if p[0] < 255*0.5])
-# print "Area of pigeon is %spx" % "{:,}".format(pigeonArea)
-
-# get pigeon bounds
-# pPixels = [(int(i%width), int(i/width)) for i, p in enumerate(pData) if p[0] < 255*0.5]
-# pxs = [p[0] for p in pPixels]
-# pys = [p[1] for p in pPixels]
-# pxBound = (min(pxs), max(pxs))
-# pyBound = (min(pys), max(pys))
-# pW = pxBound[1] - pxBound[0]
-# pH = pyBound[1] - pyBound[0]
-# print "Pigeon dimensions: %s x %spx, offset: %s x %spx" % ("{:,}".format(pW), "{:,}".format(pH), pxBound[0], pyBound[0])
-
 # get graph data
 nodeCounter = {}
 nodes = []
 links = []
+users = []
 with open(GRAPH_FILE) as f:
     graph = json.load(f)
     # normalize data
@@ -89,6 +80,7 @@ with open(GRAPH_FILE) as f:
         s = l["source"]
         t = l["target"]
         links.append((s, t))
+        users.append(l["user"])
     # count frequencies of nodes
     # nodeCounter = collections.Counter(nodes)
     # links = random.sample(links, 100)
@@ -103,57 +95,75 @@ linkCount = len(links)
 edges = [(nodes.index(l[0]), nodes.index(l[1])) for l in links]
 print "Read graph with %s nodes and %s links" % ("{:,}".format(nodeCount), "{:,}".format(linkCount))
 
-# get ideal distance k between two vertices
-# k = C * math.sqrt(1.0 * pigeonArea / nodeCount)
-# print "Ideal distance between two vertices: %spx" % k
-# print "5 most common nodes:"
-# print nodeCounter.most_common(5)
-# print "First 5 nodes:"
-# for n in nodes[:5]:
-#     pprint(n)
-# print "First 5 links"
-# for l in links[:5]:
-#     pprint(l)
-
-# build graph
-print "Building graph, may take a while..."
-g = Graph()
-g.add_vertices(nodeCount)
-g.add_edges(edges)
-layout = g.layout(LAYOUT_ALGORITHM)
-
-# get bounds and convert to pixels
-xs = [p[0] for p in layout]
-ys = [p[1] for p in layout]
-xBound = (min(xs), max(xs))
-yBound = (min(ys), max(ys))
+# config/init graph
+preProcessedFilename = "preprocessed_%s.json" % LAYOUT_ALGORITHM
 labels = []
 cWidth = width - MARGIN * 2
 cHeight = height - MARGIN * 2
-for i, p in enumerate(layout):
-    nx = norm(p[0], xBound)
-    ny = norm(p[1], yBound)
-    x = int(round(nx * cWidth + MARGIN))
-    y = int(round(ny * cHeight + MARGIN))
-    pigeon = False
-    pi = int(y * width + x)
-    if pData[pi][0] < 255 * 0.5:
-        pigeon = True
-    labels.append({
-        "label": class2Label(nodes[i]),
-        "pigeon": pigeon,
-        "point": (x, y)
-    })
+
+# pre-processed data found, just read it directly
+if os.path.isfile(preProcessedFilename):
+    print "Using preprocessed data..."
+    with open(preProcessedFilename) as f:
+        layout = json.load(f)
+        for l in layout:
+            i = l[0]
+            nx = l[1]
+            ny = l[2]
+            x = int(round(nx * cWidth + MARGIN))
+            y = int(round(ny * cHeight + MARGIN))
+            pigeon = False
+            pi = int(y * width + x)
+            if pData[pi][0] < 255 * 0.5:
+                pigeon = True
+            labels.append({
+                "label": class2Label(nodes[i]),
+                "pigeon": pigeon,
+                "point": (x, y)
+            })
+
+# process graph from scratch
+else:
+    # build graph
+    print "Building graph, may take a while..."
+    g = Graph()
+    g.add_vertices(nodeCount)
+    g.add_edges(edges)
+    layout = g.layout(LAYOUT_ALGORITHM)
+
+    # get bounds and convert to pixels
+    xs = [p[0] for p in layout]
+    ys = [p[1] for p in layout]
+    xBound = (min(xs), max(xs))
+    yBound = (min(ys), max(ys))
+
+    preprocessedData = []
+    for i, p in enumerate(layout):
+        nx = norm(p[0], xBound)
+        ny = norm(p[1], yBound)
+        x = int(round(nx * cWidth + MARGIN))
+        y = int(round(ny * cHeight + MARGIN))
+        pigeon = False
+        pi = int(y * width + x)
+        if pData[pi][0] < 255 * 0.5:
+            pigeon = True
+        labels.append({
+            "label": class2Label(nodes[i]),
+            "pigeon": pigeon,
+            "point": (x, y)
+        })
+        preprocessedData.append([i, nx, ny])
 
 # now draw the links
 if EDGES:
     lines = []
-    for edge in edges:
-        p1i = edge[0]
-        p2i = edge[1]
-        p1 = labels[p1i]["point"]
-        p2 = labels[p2i]["point"]
-        lines.append([p1, p2])
+    for i, edge in enumerate(edges):
+        if not USER_EDGES or (USER_EDGES and users[i]):
+            p1i = edge[0]
+            p2i = edge[1]
+            p1 = labels[p1i]["point"]
+            p2 = labels[p2i]["point"]
+            lines.append([p1, p2])
 
 # draw output file
 print "About to output file"
@@ -163,7 +173,7 @@ out = Image.new('RGB', (width, height), (255,255,255))
 d = ImageDraw.Draw(out)
 if EDGES:
     for line in lines:
-        d.line(line, fill=(200,200,200), width=1)
+        d.line(line, fill=(100,100,100), width=1)
 for l in labels:
     f = fnt
     if l["pigeon"]:
@@ -175,3 +185,9 @@ for l in labels:
     d.text(tp, l["label"], font=f, fill=(0,0,0))
 out.save(OUTPUT_FILE, "PNG")
 print "Saved to file %s" % OUTPUT_FILE
+
+
+if not os.path.isfile(preProcessedFilename):
+    print "Dumping graph data into json..."
+    with open(preProcessedFilename, 'w') as f:
+        json.dump(preprocessedData, f)
